254085040abd23641f2591a0d3a4ca7560c58aaf
Sarthak Shelar <sarthakshelar95@gmail.com>
2026-01-03 18:54:59 +0530

    First commit

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  title = 'Greeting App';
+  username: string = '';
+}
